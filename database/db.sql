6f195be26060ff01658c2a019e5192f5297a4968
luisgdoliveira-008 <luis_gd_oliveira@estudante.sesisenai.org.br>
2026-08-18 08:05:25 -0300

    add - database

--- database/db.sql
+++ database/db.sql
@@ -1,1 +1,14 @@
+CREATE DATABASE Sistema_pratos_LP;
+USE Sistema_pratos_LP;
 
+CREATE TABLE usuario (
+    nome VARCHAR(200) NOT NULL,
+    email VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE prato (
+    nome VARCHAR(200) NOT NULL,
+    descricao VARCHAR(200) NOT NULL,
+    preco DECIMAL(10, 2) NOT NULL,
+    categoria VARCHAR(200) NOT NULL
+);

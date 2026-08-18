7d29906dad74cd41e8b914c1ec097935496fda86
luisgdoliveira-008 <luis_gd_oliveira@estudante.sesisenai.org.br>
2026-08-18 08:11:42 -0300

    add - ajuste database

--- database/db.sql
+++ database/db.sql
@@ -2,13 +2,17 @@ CREATE DATABASE Sistema_pratos_LP;
 USE Sistema_pratos_LP;
 
 CREATE TABLE usuario (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(200) NOT NULL,
     email VARCHAR(200) NOT NULL
 );
 
 CREATE TABLE prato (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_usuario INT,
     nome VARCHAR(200) NOT NULL,
     descricao VARCHAR(200) NOT NULL,
     preco DECIMAL(10, 2) NOT NULL,
-    categoria VARCHAR(200) NOT NULL
+    categoria VARCHAR(200) NOT NULL,
+ FOREIGN KEY (id_usuario) REFERENCES usuario(id)
 );
